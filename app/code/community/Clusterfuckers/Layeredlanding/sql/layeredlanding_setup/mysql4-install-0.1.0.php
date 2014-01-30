3bc7166c2774a12426a6167049b27ffda976e2cb
Sander Mangel <mangel@redkiwi.nl>
2014-01-30 01:42:48 +0100

    added SQL update

--- app/code/community/Clusterfuckers/Layeredlanding/sql/layeredlanding_setup/mysql4-install-0.1.0.php
+++ app/code/community/Clusterfuckers/Layeredlanding/sql/layeredlanding_setup/mysql4-install-0.1.0.php
@@ -10,12 +10,13 @@ CREATE TABLE IF NOT EXISTS `{$this->getTable('layeredlanding')}` (
   `layeredlanding_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `meta_title` varchar(255) NOT NULL,
   `meta_keywords` varchar(255) NOT NULL,
-  `meta_description` text NOT NULL default '',
+  `meta_description` text NOT NULL,
   `page_title` varchar(255) NOT NULL,
-  `page_description` text NOT NULL default '',
-  `store_id` smallint(6) NOT NULL default '0',
+  `page_description` text NOT NULL,
+  `store_ids` varchar(255) NOT NULL DEFAULT '0',
+  `category_ids` varchar(255) NOT NULL DEFAULT '0',
   PRIMARY KEY (`layeredlanding_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 CREATE TABLE IF NOT EXISTS `{$this->getTable('layeredlanding/attributes')}` (
   `layeredlanding_attributes_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
