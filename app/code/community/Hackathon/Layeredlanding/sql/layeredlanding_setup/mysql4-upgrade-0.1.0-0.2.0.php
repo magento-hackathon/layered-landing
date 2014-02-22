8626038a5564b9f5292a36696ece129211f3999b
sandermangel <sander@sandermangel.nl>
2014-02-22 17:44:21 +0100

    Added unique key url,storeids to 0.2.0 update script

--- app/code/community/Hackathon/Layeredlanding/sql/layeredlanding_setup/mysql4-upgrade-0.1.0-0.2.0.php
+++ app/code/community/Hackathon/Layeredlanding/sql/layeredlanding_setup/mysql4-upgrade-0.1.0-0.2.0.php
@@ -8,6 +8,8 @@ $installer->run("
  
 ALTER TABLE  `{$this->getTable('layeredlanding')}` ADD  `page_url` VARCHAR( 255 ) NOT NULL AFTER  `page_description`;
 
+ALTER TABLE `{$this->getTable('layeredlanding')}` ADD UNIQUE KEY `uk_url_store` (`page_url`, `store_ids`);
+
     ");
  
 $installer->endSetup();
