fa827a458bda81c6d04237666743963b24bab7fc
sandermangel <sander@sandermangel.nl>
2014-02-23 11:57:40 +0100

    cleaned up commented code

--- app/code/community/Hackathon/Layeredlanding/Model/Options/Categories.php
+++ app/code/community/Hackathon/Layeredlanding/Model/Options/Categories.php
@@ -22,25 +22,6 @@ class Hackathon_Layeredlanding_Model_Options_Categories extends Mage_Core_Model_
 		}
 	 
 		return $categories;
-/*		
-        $collection = Mage::getModel('catalog/category')->getCollection()
-            ->addAttributeToSelect('name')
-            ->addAttributeToSelect('children_count')
-            ->addAttributeToSelect('level')
-            ->addAttributeToFilter('is_active', 1)
-            ->addAttributeToFilter('is_anchor', 1)
-            ->addAttributeToFilter('level', array('gt' => 1))
-            ->setOrder('level', 'ASC');
-
-        foreach ($collection as $item) {
-            $categories[] = array(
-                'label' => str_repeat('-', $item->getLevel()) . " {$item->getName()}",
-                'value' => $item->getId()
-            );
-        }
-
-        return $categories;
-*/
     }
 	
 	
