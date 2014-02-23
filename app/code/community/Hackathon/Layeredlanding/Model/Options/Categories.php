055dd3e0d2fe52683d91939b6c4e3b22c286db61
sandermangel <sander@sandermangel.nl>
2014-02-23 11:55:19 +0100

    improved admin category dropdown

--- app/code/community/Hackathon/Layeredlanding/Model/Options/Categories.php
+++ app/code/community/Hackathon/Layeredlanding/Model/Options/Categories.php
@@ -5,6 +5,24 @@ class Hackathon_Layeredlanding_Model_Options_Categories extends Mage_Core_Model_
 
     public function toOptionArray()
     {
+		$categoriesArray = Mage::getModel('catalog/category')
+			->getCollection()
+			->addAttributeToSelect('name')
+			->addAttributeToSelect('level')
+			->addAttributeToSort('path', 'asc')
+			->addFieldToFilter('is_active', array('eq'=>'1'))
+			->addFieldToFilter('level', array('gt'=>'1'))
+			->load()
+			->toArray();
+	 
+		foreach ($categoriesArray as $categoryId => $category) 
+		{
+			if (!isset($category['name'])) continue;
+			$categories[] = array('value' => $categoryId,'label' => str_repeat('.', $category['level']) . ' ' . $category['name']);
+		}
+	 
+		return $categories;
+/*		
         $collection = Mage::getModel('catalog/category')->getCollection()
             ->addAttributeToSelect('name')
             ->addAttributeToSelect('children_count')
@@ -22,5 +40,8 @@ class Hackathon_Layeredlanding_Model_Options_Categories extends Mage_Core_Model_
         }
 
         return $categories;
+*/
     }
+	
+	
 }
