105863e0fedecd4e5be673a4fa399e355826be55
sandermangel <sander@sandermangel.nl>
2014-02-23 15:58:39 +0100

    added wysiwyg to page description ad removed whitespace in form blocks

--- app/code/community/Hackathon/Layeredlanding/Block/Adminhtml/Layeredlanding/Edit/Tab/Content.php
+++ app/code/community/Hackathon/Layeredlanding/Block/Adminhtml/Layeredlanding/Edit/Tab/Content.php
@@ -2,85 +2,70 @@
 
 class Hackathon_Layeredlanding_Block_Adminhtml_Layeredlanding_Edit_Tab_Content extends Mage_Adminhtml_Block_Widget_Form
 {
+	protected function _prepareLayout()
+    {
+        $return = parent::_prepareLayout();
+        if (Mage::getSingleton('cms/wysiwyg_config')->isEnabled()) 
+		{
+            $this->getLayout()->getBlock('head')->setCanLoadTinyMce(true);
+        }
+        return $return;
+    }
+	
     protected function _prepareForm()
     {
         $form = new Varien_Data_Form();
         $this->setForm($form);
-        $fieldset = $form->addFieldset(
-            'layeredlanding_form',
-            array(
-                'legend' => Mage::helper('layeredlanding')->__('Landingpage Content'),
-                'class' => 'fieldset-wide'
-            )
-        );
+        
+		$fieldset = $form->addFieldset('layeredlanding_form', array(
+			'legend' => Mage::helper('layeredlanding')->__('Landingpage Content'),
+			'class' => 'fieldset-wide'
+		));
 
-        $fieldset->addField(
-            'page_url',
-            'text',
-            array(
-                'label' => Mage::helper('layeredlanding')->__('Page URL'),
-                'class' => 'required-entry',
-                'required' => true,
-                'name' => 'page_url',
-            )
-        );
+        $fieldset->addField('page_url', 'text', array(
+			'label' => Mage::helper('layeredlanding')->__('Page URL'),
+			'class' => 'required-entry',
+			'required' => true,
+			'name' => 'page_url',
+		));
 
-        $fieldset->addField(
-            'page_title',
-            'text',
-            array(
-                'label' => Mage::helper('layeredlanding')->__('Page title'),
-                'class' => 'required-entry',
-                'required' => true,
-                'name' => 'page_title',
-            )
-        );
+        $fieldset->addField('page_title', 'text', array(
+			'label' => Mage::helper('layeredlanding')->__('Page title'),
+			'class' => 'required-entry',
+			'required' => true,
+			'name' => 'page_title',
+		));
 
-        $fieldset->addField(
-            'page_description',
-            'textarea',
-            array(
-                'label' => Mage::helper('layeredlanding')->__('Page description'),
-                'class' => 'required-entry',
-                'required' => true,
-                'name' => 'page_description',
-				'style' => "width:500px;",
-            )
-        );
+        $fieldset->addField('page_description', 'editor', array(
+			'label' => Mage::helper('layeredlanding')->__('Page description'),
+			'class' => 'required-entry',
+			'required' => true,
+			'name' => 'page_description',
+			'style' => "width:500px;height:350px;",
+			'config' => Mage::getSingleton('cms/wysiwyg_config')->getConfig(),
+		));
 
-        $fieldset->addField(
-            'meta_title',
-            'text',
-            array(
-                'label' => Mage::helper('layeredlanding')->__('Meta title'),
-                'class' => 'required-entry',
-                'required' => true,
-                'name' => 'meta_title',
-            )
-        );
+        $fieldset->addField('meta_title', 'text', array(
+			'label' => Mage::helper('layeredlanding')->__('Meta title'),
+			'class' => 'required-entry',
+			'required' => true,
+			'name' => 'meta_title',
+		));
 
-        $fieldset->addField(
-            'meta_keywords',
-            'text',
-            array(
-                'label' => Mage::helper('layeredlanding')->__('Meta keywords'),
-                'class' => 'required-entry',
-                'required' => true,
-                'name' => 'meta_keywords',
-            )
-        );
+        $fieldset->addField('meta_keywords', 'text', array(
+			'label' => Mage::helper('layeredlanding')->__('Meta keywords'),
+			'class' => 'required-entry',
+			'required' => true,
+			'name' => 'meta_keywords',
+		));
 
-        $fieldset->addField(
-            'meta_description',
-            'textarea',
-            array(
-                'label' => Mage::helper('layeredlanding')->__('Meta description'),
-                'class' => 'required-entry',
-                'required' => true,
-                'name' => 'meta_description',
-				'style' => "width:500px;",
-            )
-        );
+        $fieldset->addField('meta_description', 'textarea', array(
+			'label' => Mage::helper('layeredlanding')->__('Meta description'),
+			'class' => 'required-entry',
+			'required' => true,
+			'name' => 'meta_description',
+			'style' => "width:500px;",
+		));
 
         if (Mage::getSingleton('adminhtml/session')->getLayeredlandingData()) {
             $data = Mage::getSingleton('adminhtml/session')->getLayeredlandingData();
--- app/code/community/Hackathon/Layeredlanding/Block/Adminhtml/Layeredlanding/Edit/Tab/Design.php
+++ app/code/community/Hackathon/Layeredlanding/Block/Adminhtml/Layeredlanding/Edit/Tab/Design.php
@@ -6,60 +6,42 @@ class Hackathon_Layeredlanding_Block_Adminhtml_Layeredlanding_Edit_Tab_Design ex
     {
         $form = new Varien_Data_Form();
         $this->setForm($form);
-        $fieldset = $form->addFieldset(
-            'layeredlanding_form',
-            array(
-                'legend' => Mage::helper('layeredlanding')->__('Landingpage Design'),
-                'class' => 'fieldset-wide'
-            )
-        );
+		
+        $fieldset = $form->addFieldset('layeredlanding_form', array(
+			'legend' => Mage::helper('layeredlanding')->__('Landingpage Design'),
+			'class' => 'fieldset-wide'
+		));
 
-        $fieldset->addField(
-            'display_layered_navigation',
-            'select',
-            array(
-                'label' => Mage::helper('layeredlanding')->__('Display Layered Navigation'),
-                'class' => 'required-entry',
-                'required' => true,
-                'name' => 'display_layered_navigation',
-                'values' => Mage::getSingleton('layeredlanding/options_boolean')->toOptionArray(),
-            )
-        );
+        $fieldset->addField('display_layered_navigation', 'select', array(
+			'label' => Mage::helper('layeredlanding')->__('Display Layered Navigation'),
+			'class' => 'required-entry',
+			'required' => true,
+			'name' => 'display_layered_navigation',
+			'values' => Mage::getSingleton('layeredlanding/options_boolean')->toOptionArray(),
+		));
 
-        $fieldset->addField(
-            'display_in_top_navigation',
-            'select',
-            array(
-                'label' => Mage::helper('layeredlanding')->__('Display in Top Navigation'),
-                'class' => 'required-entry',
-                'required' => true,
-                'name' => 'display_in_top_navigation',
-                'values' => Mage::getSingleton('layeredlanding/options_boolean')->toOptionArray(),
-                'value' => '0',
-            )
-        );
+        $fieldset->addField('display_in_top_navigation', 'select', array(
+			'label' => Mage::helper('layeredlanding')->__('Display in Top Navigation'),
+			'class' => 'required-entry',
+			'required' => true,
+			'name' => 'display_in_top_navigation',
+			'values' => Mage::getSingleton('layeredlanding/options_boolean')->toOptionArray(),
+			'value' => '0',
+		));
 
-        $fieldset->addField(
-            'custom_layout_template',
-            'select',
-            array(
-                'label' => Mage::helper('layeredlanding')->__('Page Layout'),
-                'required' => false,
-                'name' => 'custom_layout_template',
-                'values' => Mage::getSingleton('layeredlanding/options_layout')->toOptionArray(),
-            )
-        );
+        $fieldset->addField('custom_layout_template', 'select', array(
+			'label' => Mage::helper('layeredlanding')->__('Page Layout'),
+			'required' => false,
+			'name' => 'custom_layout_template',
+			'values' => Mage::getSingleton('layeredlanding/options_layout')->toOptionArray(),
+		));
 
-        $fieldset->addField(
-            'custom_layout_update',
-            'textarea',
-            array(
-                'label' => Mage::helper('layeredlanding')->__('Custom Layout Update'),
-                'required' => false,
-                'name' => 'custom_layout_update',
-				'style' => "width:500px;",
-            )
-        );
+        $fieldset->addField('custom_layout_update', 'textarea', array(
+			'label' => Mage::helper('layeredlanding')->__('Custom Layout Update'),
+			'required' => false,
+			'name' => 'custom_layout_update',
+			'style' => "width:500px;",
+		));
 
         if (Mage::getSingleton('adminhtml/session')->getLayeredlandingData()) {
             $data = Mage::getSingleton('adminhtml/session')->getLayeredlandingData();
