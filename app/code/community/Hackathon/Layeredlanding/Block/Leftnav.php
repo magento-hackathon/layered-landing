9248cf467905e5651c9a90b9b490e8edab83aa98
Matthias Kleine <matthias@klein0r.de>
2014-02-24 11:36:16 +0100

    Moved Store Filter implementation to Collection

--- app/code/community/Hackathon/Layeredlanding/Block/Leftnav.php
+++ app/code/community/Hackathon/Layeredlanding/Block/Leftnav.php
@@ -4,12 +4,12 @@ class Hackathon_Layeredlanding_Block_Leftnav extends Mage_Core_Block_Template
 {
 	public function getPagesList()
 	{
-		$collection = Mage::getModel('layeredlanding/layeredlanding')->getCollection()
+        /** @var $collection Hackathon_Layeredlanding_Model_Mysql4_Layeredlanding_Collection */
+        $collection = Mage::getModel('layeredlanding/layeredlanding')->getCollection()
 			->addFieldToSelect('page_title')
-			->addFieldToSelect('page_url');
-			
-		Mage::getModel('layeredlanding/layeredlanding')->addStoreFilter($collection);
-		
+			->addFieldToSelect('page_url')
+            ->addStoreFilter();
+
 		return $collection;
 	}
 }
--- app/code/community/Hackathon/Layeredlanding/Model/Mysql4/Layeredlanding/Collection.php
+++ app/code/community/Hackathon/Layeredlanding/Model/Mysql4/Layeredlanding/Collection.php
@@ -7,4 +7,15 @@ class Hackathon_Layeredlanding_Model_Mysql4_Layeredlanding_Collection extends Ma
         //parent::__construct();
         $this->_init('layeredlanding/layeredlanding');
     }
+
+    public function addStoreFilter($store_id = null)
+    {
+        if (is_null($store_id)) {
+            $store_id = Mage::app()->getStore()->getId();
+        }
+
+        $this->getSelect()->where(" (CONCAT(',', `store_ids`, ',') LIKE '%,{$store_id},%' OR `store_ids` = 0)");
+
+        return $this;
+    }
 }
