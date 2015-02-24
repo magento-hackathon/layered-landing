981e09cfda64f67d6eafccfbd54d72f12520057f
Peter Jaap Blaakmeer <peterjaap@elgentos.nl>
2015-02-24 10:04:47 +0100

    Put complete category path name in adminhtml dropdown to identify categories more easily

--- app/code/community/Hackathon/Layeredlanding/Model/Options/Categories.php
+++ app/code/community/Hackathon/Layeredlanding/Model/Options/Categories.php
@@ -5,24 +5,42 @@ class Hackathon_Layeredlanding_Model_Options_Categories extends Mage_Core_Model_
 
     public function toOptionArray()
     {
-		$categoriesArray = Mage::getModel('catalog/category')
-			->getCollection()
-			->addAttributeToSelect('name')
-			->addAttributeToSelect('level')
-			->addAttributeToSort('path', 'asc')
-			->addFieldToFilter('is_active', array('eq'=>'1'))
-			->addFieldToFilter('level', array('gt'=>'1'))
-			->load()
-			->toArray();
-	 
-		foreach ($categoriesArray as $categoryId => $category) 
-		{
-			if (!isset($category['name'])) continue;
-			$categories[] = array('value' => $categoryId,'label' => str_repeat('.', $category['level']) . ' ' . $category['name']);
-		}
-	 
-		return $categories;
+        $categories = array();
+
+        $allCategoriesCollection = Mage::getModel('catalog/category')
+            ->getCollection()
+            ->addAttributeToSelect('name')
+            ->addFieldToFilter('level', array('gt'=>'0'));
+
+        $allCategoriesArray = $allCategoriesCollection->load()->toArray();
+
+        $categoriesArray = $allCategoriesCollection
+            ->addAttributeToSelect('level')
+            ->addAttributeToSort('path', 'asc')
+            ->addFieldToFilter('is_active', array('eq'=>'1'))
+            ->addFieldToFilter('level', array('gt'=>'1'))
+            ->load()
+            ->toArray();
+
+        foreach ($categoriesArray as $categoryId => $category)
+        {
+            if (!isset($category['name'])) {
+                continue;
+            }
+            $categoryIds = explode('/', $category['path']);
+            $nameParts = array();
+            foreach($categoryIds as $catId) {
+                if($catId == 1) {
+                    continue;
+                }
+                $nameParts[] = $allCategoriesArray[$catId]['name'];
+            }
+            $categories[] = array(
+                'value' => $categoryId,
+                'label' => implode(' / ', $nameParts)
+            );
+        }
+
+        return $categories;
     }
-	
-	
 }
